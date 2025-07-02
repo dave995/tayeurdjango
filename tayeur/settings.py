78fb6fe5fe57ef1989dc4c01c3dc9114c2e7a080
daouda thiam <daoudathiam95@gmail.com>
2025-07-02 15:09:50 +0000

    correct virgul

--- tayeur/settings.py
+++ tayeur/settings.py
@@ -110,7 +110,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://tayeurdjango-3.onrender.com",
-    "https://tayeurgestions.netlify.app"
+    "https://tayeurgestions.netlify.app",
     "https://tayeurgestionv2.netlify.app"
 ]
 
