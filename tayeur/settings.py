066d02d65885fcb7f38618e1468cd289e6d5ac04
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2025-08-07 12:09:48 +0000

    Fix
    Activer PostgreSQL en production

--- tayeur/settings.py
+++ tayeur/settings.py
@@ -72,20 +72,12 @@ WSGI_APPLICATION = 'tayeur.wsgi.application'
 
 # Configuration de la base de données
 # LOCAL (Développement) - SQLite
-DATABASES = {
-        # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('DB_NAME', 'tayeur_db'),
-    #     'USER': os.getenv('DB_USER', 'postgres'),
-    #     'PASSWORD': os.getenv('DB_PASSWORD', ''),
-    #     'HOST': os.getenv('DB_HOST', 'localhost'),
-    #     'PORT': os.getenv('DB_PORT', '5432'),
-    # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # PRODUCTION (Render + NeonDB) - PostgreSQL
 # Pour déployer en production :
